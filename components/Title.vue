d36afe3c454dbdc322aa16d890ed9e6965bb066b
llchimerall <chimera-i@bk.ru>
2018-09-12 21:46:23 +0300

    Title2

--- components/Title.vue
+++ components/Title.vue
@@ -10,6 +10,20 @@
 
 <style scoped>
   .title {
+   color: white;
+   font-size: 1em;
+  }
 
+  .title:after {
+    margin-top: 15px;
+    max-width: 265px;
+    display: block;
+    content: '';
+    height: 7px;
+    background: #691eaf;
+    background: -moz-linear-gradient(left, #691eaf 0%, #711294 0%, #7e14a4 47%, #ba6ad9 100%);
+    background: -webkit-linear-gradient(left, #691eaf 0%,#711294 0%,#7e14a4 47%,#ba6ad9 100%);
+    background: linear-gradient(to right, #691eaf 0%,#711294 0%,#7e14a4 47%,#ba6ad9 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#691eaf', endColorstr='#ba6ad9',GradientType=1 );
   }
 </style>
