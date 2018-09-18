5a160bd330796e2223cb5adfe39e4258551657ea
llchimerall <chimera-i@bk.ru>
2018-09-18 19:34:22 +0300

    add feedItem.vue

---
+++ components/FeedItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <li>
+    <p class="player">
+      New player Bone_Crusher24
+    </p>
+    <p class="action">
+    join
+    </p>
+    <p class="name_action">
+    CS:Go League
+    </p>
+    <p class="time">
+    1m ago
+    </p>
+    </li>
+</template>
+
+
+<script>
+	export default {
+		props: ['text']
+	}
+</script>
+
+<style scoped>
+ 
+</style>
