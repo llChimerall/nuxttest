25736d9f5a77f7ede0f5a6f22f08f885a2f870d0
llchimerall <chimera-i@bk.ru>
2018-10-03 22:47:21 +0300

    gradient2

--- components/Bsignup.vue
+++ components/Bsignup.vue
@@ -4,9 +4,9 @@
     :class="{'m-blur': blur}"
     :style="{'background-image': `url(${background})`}"
   >
+    <div class="gradient"></div>
     <div class="more"><a href="#">Read more</a></div>
     <div class="bchoose"><Button text="Sign up" :gray="true" /></div>
-    <div class="gradient"></div>
   </div>
 </template>
 
@@ -37,6 +37,7 @@ import Button from '~/components/Button.vue';
   margin-right: 15px;
 }
 .back {
+  position: relative;
   margin-top: 30px;
   height: 270px;
   width: 220px;
@@ -59,19 +60,22 @@ import Button from '~/components/Button.vue';
 }
 .bchoose {
   display: block;
-  margin-top: 185px;
+  margin-top: 195px;
   text-align: center;
+  position: relative;
 }
 .gradient {
-  display: inline-block;
-  height: 270px;
-  width: 220px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
   background-position: center;
   border-radius: 10px;
   border: 0px solid #691eaf;
-  background: -moz-linear-gradient(top, rgba(134,64,197,0) 1%, rgba(134,64,197,0) 52%, rgba(134,64,197,0) 65%, rgba(134,64,197,0.63) 99%, rgba(134,64,197,0.65) 100%);
-  background: -webkit-linear-gradient(top, rgba(134,64,197,0) 1%,rgba(134,64,197,0) 52%,rgba(134,64,197,0) 65%,rgba(134,64,197,0.63) 99%,rgba(134,64,197,0.65) 100%);
-  background: linear-gradient(to bottom, rgba(134,64,197,0) 1%,rgba(134,64,197,0) 52%,rgba(134,64,197,0) 65%,rgba(134,64,197,0.63) 99%,rgba(134,64,197,0.65) 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#008640c5', endColorstr='#a68640c5',GradientType=0 );
+  background: -moz-linear-gradient(top, rgba(134,64,197,0) 2%, rgba(155,80,204,0) 46%, rgba(158,82,205,0) 52%, rgba(176,97,212,0.65) 91%, rgba(180,100,213,0.65) 100%);
+  background: -webkit-linear-gradient(top, rgba(134,64,197,0) 2%,rgba(155,80,204,0) 46%,rgba(158,82,205,0) 52%,rgba(176,97,212,0.65) 91%,rgba(180,100,213,0.65) 100%);
+  background: linear-gradient(to bottom, rgba(134,64,197,0) 2%,rgba(155,80,204,0) 46%,rgba(158,82,205,0) 52%,rgba(176,97,212,0.65) 91%,rgba(180,100,213,0.65) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#008640c5', endColorstr='#a6b464d5',GradientType=0 );
 }
 </style>
